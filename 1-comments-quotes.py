89fc9722572f5cb5a7be3d817a305a54d75485ac
codebyuzair <uzairmanagi@gmail.com>
2025-01-14 09:48:17 +0500

    list/array added

--- 1-comments-quotes.py
+++ 1-comments-quotes.py
@@ -1,7 +1,30 @@
 # hashtag is used for single line comment
 
 """
-3 commas are 
+3 double quotes are 
 used for
 multi line comments.
 """
+
+'''
+3 single quotes are 
+also used for 
+multi line comments 
+'''
+
+# Anything in double or single quotes are string
+friuts = "Apple"
+vegetable = 'Carrot'
+
+print(friuts)
+print(vegetable)
+
+# 3 quotes in print statements are used for paragraph
+print("""
+      this is used 
+      for paragraph
+      is python
+      """)
+
+
+
---
+++ 3-datatypes.py
@@ -0,0 +1,15 @@
+# String's
+str1 = "alpha"
+str2 = 'beta'
+str3 = '''gamma string'''
+str4 = """delta string"""
+
+# Numbers 
+num1 = 125
+flt1 = 1.2
+
+# List/Array(Collection of multi datatype, enclosed in square brackets)
+list1 = [str1, "Devops", 47, num1, 1.5]
+
+# Printing the above List/Array
+print(list1) 
