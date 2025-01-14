ee1c95277a3a2503468f15b25fe6aca00957beaf
codebyuzair <uzairmanagi@gmail.com>
2025-01-14 09:25:33 +0500

    added vars, datatypes and comments

---
+++ 1-comments-quotes.py
@@ -0,0 +1,7 @@
+# hashtag is used for single line comment
+
+"""
+3 commas are 
+used for
+multi line comments.
+"""
---
+++ 3-variables.py
@@ -0,0 +1,14 @@
+#initializing Variables
+var1 = "ali"
+var2 = 32
+var3 = 3.4
+
+#Printing variables valuess
+print(var1)
+print(var2)
+print(var3)
+
+# Checking variables typs
+print(type(var1))
+print(type(var2))
+print(type(var3))
