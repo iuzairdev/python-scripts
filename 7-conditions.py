80fb16cfcbd9eadf7ed19373d22f7ac806dd9fd9
codebyuzair <uzairmanagi@gmail.com>
2025-01-14 21:40:30 +0500

    Added conditions file

---
+++ 7-conditions.py
@@ -0,0 +1,19 @@
+# If Condition
+# x = 21
+
+# if (x < 2):
+#     print("hello")
+# else:
+#     print("Hi")
+
+# If/Elif Condition
+x = 40
+
+if x > 40:
+    print("X Is greather")
+elif x == 40:
+    print("x is equal")    
+else:
+    print("X is less")
+
+    
