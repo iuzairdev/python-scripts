fa0234e1d6b42106bdfe6654b12c488e30701414
iuzairdev <uzairmanagi@gmail.com>
2025-01-19 08:02:23 +0500

    Loops Added to python scripts

---
+++ 9-loops.py
@@ -0,0 +1,11 @@
+# If you want to execute something repetitively so 
+# for that we use loops 
+
+# FOR LOOP
+
+planet = "Earth"
+for i in planet:
+    print("Value of i is now, "i)
+
+print("Rest of the Code")
+
